f582ea8942173752565043d6c189991293b708f5
androvisck <androvisck@gmail.com>
2025-12-13 16:50:38 -0400

    corrigimdo a concordância do texto

--- app.js
+++ app.js
@@ -10,16 +10,31 @@ while (chute != numeroSecreto){
     chute = prompt('Digite um número entre 1 e 10: ');
 
     if (chute == numeroSecreto) {
-        alert(`Parabéns, você acertou o número secreto ${numeroSecreto}`);
+
+        break;
+
     } else {
+
         if (chute > numeroSecreto) {
+
             alert(`O número secreto é menor que ${chute}.`);
+
         } else{
+
             alert(`O número secreto é maior que ${chute}.`);
+
         }
     }
 
     tentativa++
 }
 
-alert(`Número de tentativas: ${tentativa}.`)
+if (tentativa == 1) {
+
+    alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativa} tentativa.`);
+
+} else {
+    
+    alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativa} tentativas.`);
+
+}
