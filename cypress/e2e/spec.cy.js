b3f52d4a582a8e5166c971ce94c6cdf5e697e0eb
androvisck <androvisck@gmail.com>
2025-12-25 11:04:01 -0400

    fisrt test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
 describe('template spec', () => {
   it('passes', () => {
+    
     cy.visit('https://adopet-frontend-cypress.vercel.app');
+    
+    cy.get('a[data-test="register-button"]').click();
+    
+    cy.get('input[name="nome"]').type('Escrite Machado de Assis');
+    
+    cy.get('input[name="email"]').type('escritor_machado_assis@gmail.com');
+    
+    cy.get('input[name="password"]').type('As12345!@#');
+    
+    cy.get('input[name="confirm_password"]').type('As12345!@#');
+  
+    cy.get('button[data-test="submit-button"]').click();
+
   })
 })
